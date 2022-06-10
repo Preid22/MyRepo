1ca0e76a25cba879e104f0d81605bf5cc7b147e9
Patrick Reid <preid22gmail.com>
2022-06-10 09:11:36 -0400

    A few notes

---
+++ class_object_notes.js
@@ -0,0 +1,45 @@
+function Employee() {
+         this.name = '';
+        this.dept = 'general';
+    }
+
+
+function Manager() {
+    Employee.call(this);
+    this.reports = [];
+}
+Manager.prototype = Object.create(Employee.prototype);
+Manager.prototype.constructor = Manager;
+
+function WorkerBee() {
+    Employee.call(this);
+    this.projects = [];
+}
+WorkerBee.prototype = Object.create(Employee.prototype);
+WorkerBee.prototype.constructor = WorkerBee;
+
+function SalesPerson() {
+    WorkerBee.call(this);
+    this.dept = 'sales'
+    this.quota = 100;
+}
+SalesPerson.prototype = Object.create(WorkerBee.prototype);
+SalesPerson.prototype.constructor = SalesPerson;
+
+function Engineer() {
+    WorkerBee.call(this);
+    this.dept = 'engineering';
+    this.machine = '';
+}
+Engineer.prototype = Object.create(WorkerBee.prototype);
+Engineer.prototype.constructor = Engineer;
+
+
+var Jim = new Employee;
+var Sally = new Manager;
+var Mark = new WorkerBee;
+var Fred = new SalesPerson
+var Jane = new Engineer;
+
+console.log(Jim);
+console.log(Sally);
---
+++ iteration_practice1.js
@@ -0,0 +1,146 @@
+let arr = [1,2,3,4,5,6,7,8,9,10];
+let a = [];
+let b = [];
+let c = [];
+let d = [];
+let w = [];
+let x = [];
+let y = [];
+let z = [];
+
+
+
+
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+// print the numbers 1-10
+
+// for(let i=0;i<=9;i++){
+//     console.log(arr[i]);
+// }
+//print numbers 1-10
+
+// for(let i=9;i>=0;i--){
+//     console.log(arr[i]);
+// }
+// print numbers 10-1
+
+
+
+// for(let i=4;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+// //print numbers 5-10
+
+// for(let i=1;i<arr.length;i+=2){
+//     console.log(arr[i]);
+// }
+// print 2-10 in increments of 2
+
+// for(let i=0;i != 10;i++){
+//     let output = i;
+//     console.log(output);
+// }
+//prints 0-9, stops at 10
+
+// for(let i=0;i<=arr.length;i++){
+//     if(arr[i]%2 == 0){
+//         console.log(arr[i])
+//     }
+// }
+//iterates through arr and prints even numbers
+
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]%2 == 1){
+//         console.log(arr[i]);
+//     }
+// }
+//iterates through arr and prints odd numbers
+
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]%2 == 0){
+//         x.push(arr[i]);
+//     }else{
+//         y.push(arr[i]);
+//     }
+//  }
+//  console.log(x);
+//  console.log(y);
+//iterates through arr and pushes even numbers to array x and odds to array y
+
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]<=5){
+//         x.push(arr[i]);
+//     }else{
+//         y.push(arr[i]);
+//     }
+// }
+// let output1 = x
+// let output2 = y
+// console.log(output1,output2);
+//outputs two arrays, first 1-5 then 6-10
+
+// for(let i=arr.length;i>=0;i--){
+//     if(arr[i] >=6 && arr[i]%2 == 1){
+//         a.unshift(arr[i]);
+//     }else if(arr[i]>=6 && arr[i]%2 ==0){
+//         b.unshift(arr[i]);
+//     }else if(arr[i]<=5 && arr[i]%2 == 1){
+//         c.unshift(arr[i]);
+//     }else if(arr[i]<= 5 && arr[i]%2 == 0){
+//         d.unshift(arr[i]);
+//     }
+// }
+// console.log(a, b, c, d);
+//outputs four arrays: 6-10 odd numbers, 6-10 even numbers, 1-5 odd numbers, 1-5 even numbers
+
+// for(let i=arr.length;i>=0;i--){
+//     if(arr[i]>=6 && arr[i]%2 == 1){
+//         w.push(arr[i]);
+//     }else if(arr[i]>=6 && arr[i]%2 == 0){
+//         x.push(arr[i]);
+//     }else if(arr[i]<=5 && arr[i]%2 == 1){
+//         y.push(arr[i]);
+//     }else if(arr[i]<=5 && arr[i]%2 == 0){
+//         z.push(arr[i]);
+//     }
+// }
+// console.log(w, x, y, z);
+//outputs four arrays: 10-6 odd, 10-6 even, 5-1 odd, 5-1 even
+
+// let counter = 0;
+// for(let i=0;i<25;i++){
+//     counter+=1;
+//     console.log(counter);
+// }
+//outputs 1-25, one num per line
+
+// for(let i=0;i<=100;i+=5){
+//     let output = i;
+//     console.log(output);
+// }
+//outputs 0-100 by fives
+
+// for(let i=100;i>=0;i-=10){
+//     let output = i;
+//     console.log(output);
+// }
+//outputs 100-0 by factors of ten
+
+
+// let counter = 0;
+// for(let i=0;i<25;i++){
+//     counter++;
+// }
+// console.log(counter)
+//outputs 25
+
+
+// let counter = "";
+// for(let i=0;i<10;i++){
+//     counter = counter+"*";
+//     console.log(counter);
+// }
+//outputs a little traingle of stars
+
